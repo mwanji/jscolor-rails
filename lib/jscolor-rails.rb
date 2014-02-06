c09ed662ad5c2b3eda71c1df4727174690d97bef
moandji.ezana <mwanji@gmail.com>
2014-02-06 20:33:29 +0200

    Put non-digest copies of images in public/assets

--- lib/jscolor-rails.rb
+++ lib/jscolor-rails.rb
@@ -4,7 +4,11 @@ module Jscolor
   module Rails
     class Engine < ::Rails::Engine
       initializer "jscolor.assets.precompile" do |app|
-        app.config.assets.precompile << %w(arrow.gif cross.gif hs.png hv.png)
+        app.config.assets.precompile << %w(*.gif *.png)
+      end
+      
+      rake_tasks do
+        load "tasks/assets-non-digest.rake"
       end
     end
   end
