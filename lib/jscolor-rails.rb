b27be9b72fb48046a6eaaa06c0dbb64c6d3907d4
moandji.ezana <mwanji@gmail.com>
2014-02-06 16:07:44 +0200

    Use initializer to put images in public directory

--- lib/jscolor-rails.rb
+++ lib/jscolor-rails.rb
@@ -3,6 +3,9 @@ require "jscolor-rails/version"
 module Jscolor
   module Rails
     class Engine < ::Rails::Engine
+      initializer "jscolor.assets.precompile" do |app|
+        app.config.assets.precompile << %w(arrow.gif cross.gif hs.png hv.png)
+      end
     end
   end
 end
